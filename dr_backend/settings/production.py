7d944f7fa0363d0f23b3d48a9b513a6d15788b03
Brad Rice <bradrice1@gmail.com>
2021-01-11 19:07:11 -0500

    allowed hosts to test

--- dr_backend/settings/production.py
+++ dr_backend/settings/production.py
@@ -3,12 +3,13 @@ import json
 DEBUG = True
 
 ALLOWED_HOSTS = [
-        '45.56.110.221',
-        '45.56.110.221:8080',
-        'static.dianarice.art',
-        'media.dianarice.art'
-        'dianarice.art',
-        'admin.dianarice.art',
+    *
+        # '45.56.110.221',
+        # '45.56.110.221:8081',
+        # 'static.dianarice.art',
+        # 'media.dianarice.art'
+        # 'dianarice.art',
+        # 'admin.dianarice.art',
         ]
 
 try:
