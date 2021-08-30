50a20f94a0838b4271c01a022310a669c7d5d0b6
Brad Rice <bradrice1@gmail.com>
2021-08-30 15:57:12 -0400

    update dependencies as of August 2021

--- about/apps.py
+++ about/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 
 
-class AboutpagecontnetConfig(AppConfig):
-    name = 'AboutPageContnet'
+class AboutConfig(AppConfig):
+    default_auto_field = 'django.db.models.AutoField'
+    name = 'about'
